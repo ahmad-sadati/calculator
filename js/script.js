c7712b23de885996532e7fa76c806458ae850929
Ahmad Sadati <a.sadati336@gmail.com>
2023-10-22 21:35:15 +0330

    reworked js codes

--- js/script.js
+++ js/script.js
@@ -1,103 +1,38 @@
 const display = document.querySelector(".display");
 let tempNum = "";
-let firstNum, secondNum, operator, res,total,n=0;
+let firstNum, secondNum, operator, res;
 function getNum(input) {
   tempNum += input;
   display.innerHTML = tempNum;
 }
 function sum() {
-  if(n==0){
-      display.innerHTML = "";
-  firstNum = tempNum;
+  display.innerHTML = "";
+  if (firstNum === undefined) {
+    firstNum = tempNum;
+  } else {
+    result();
+  }
   tempNum = "";
   operator = "sum";
-  // console.log(firstNum);
-  n++
-  console.log(n);
 }
-  else if(n==1){
-    display.innerHTML = ''
-    total= +firstNum + +tempNum
-    firstNum = ''
-    tempNum = ''
-    operator = "sum";
-    // console.log(firstNum);
-    // console.log(total);
-    console.log(n);
-    n++
-  }
-  else{    
-    display.innerHTML = ''
-    total= +total + +tempNum
-    tempNum = ''
-    operator = "sum";
-    // console.log(firstNum);
-    // console.log(total);
-    console.log(n);
-    n++
-  }
- 
-}
-function minus () {
-  // display.innerHTML = "";
-  // firstNum = tempNum;
-  // tempNum = "";
-  // operator = "minus";
-  if(n==0){
-    display.innerHTML = "";
-firstNum = tempNum;
-tempNum = "";
-operator = "minus";
-// console.log(firstNum);
-n++
-console.log(n);
-}
-else if(n==1){
-  display.innerHTML = ''
-  total= +firstNum - +tempNum
-  firstNum = ''
-  tempNum = ''
-  operator = "minus";
-  // console.log(firstNum);
-  // console.log(total);
-  console.log(n);
-  n++
-}
-else{    
-  display.innerHTML = ''
-  total= +total - +tempNum
-  tempNum = ''
-  operator = "minus";
-  // console.log(firstNum);
-  // console.log(total);
-  console.log(n);
-  n++
-}
-}
-function multiply () {
+function minus() {
   display.innerHTML = "";
-  firstNum = tempNum;
+  if (firstNum === undefined) {
+    firstNum = tempNum;
+  } else {
+    result();
+  }
   tempNum = "";
-  operator = "cross";
+  operator = "minus";
 }
 function result() {
   secondNum = tempNum;
-  // tempNum = "";
-  if (operator === "sum" && n==1) {
-    res = +firstNum + +secondNum}
-  else if(operator === "sum" && n>1){
-      res = +total + +secondNum
-  } 
-  else if (operator === "minus" && n==1) {
+  tempNum = "";
+  if (operator === "sum") {
+    res = +firstNum + +secondNum;
+  } else if (operator === "minus") {
     res = +firstNum - +secondNum;
   }
-  else if(operator === "minus" && n>1){
-    res = +total - +secondNum
-} 
-else if (operator === "cross") {
-  res = +firstNum * +secondNum;
-}
   display.innerHTML = res;
-  console.log(secondNum);
-  console.log(res);
+  firstNum = res;
 }
